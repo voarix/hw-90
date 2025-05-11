65f6d39186c5a87e0db3fd0dd19a907d9a4bcb3c
Kachkynov Alikhan <t8t228@gmail.com>
2025-05-12 01:15:17 +0600

    [hw-90] [api] Implement WebSocket endpoint '/draw', send and get drawings

--- backend/index.ts
+++ backend/index.ts
@@ -13,3 +13,60 @@ const router = express.Router();
 wsInstance.applyTo(router);
 
 const connectedClient: WebSocket[] = [];
+
+interface IncomingMessage {
+    type: 'DRAWING_SEND' | 'DRAWING_NEW';
+    payload: Drawing | Drawing[];
+}
+
+interface Drawing {
+    x: number;
+    y: number;
+    color: string;
+}
+
+let currentDrawing: Drawing[] = [];
+
+router.ws('/draw', (ws, _req) => {
+    connectedClient.push(ws);
+    console.log('Total connections: ', connectedClient.length);
+
+    ws.send(JSON.stringify({
+        action: 'DRAWING_SEND',
+        payload: currentDrawing,
+    }));
+
+    ws.on('message', (message) => {
+        try {
+            const decodedMessage = JSON.parse(message.toString()) as IncomingMessage;
+
+            if (decodedMessage.type === 'DRAWING_NEW') {
+                const newDrawings = Array.isArray(decodedMessage.payload) ? decodedMessage.payload : [decodedMessage.payload];
+                currentDrawing.push(...newDrawings);
+
+                newDrawings.forEach(newDrawing => {
+                    connectedClient.forEach(clientWS => {
+                        clientWS.send(JSON.stringify({
+                            type: 'DRAWING_NEW',
+                            payload: newDrawing,
+                        }));
+                    });
+                });
+            }
+        } catch (e) {
+            ws.send(JSON.stringify({error: "Invalid message"}));
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        const index = connectedClient.indexOf(ws);
+        connectedClient.splice(index, 1);
+        console.log('Total connections: ', connectedClient.length);
+    });
+});
+
+app.use(router);
+app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+});
